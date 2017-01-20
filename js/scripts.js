09b22449ab5f139e555fb699f4f9d96d536626ea
Charles Peden <ccbpeden@warpmail.net>
2017-01-20 08:59:04 -0800

    add code to scripts.js to pass 2nd spec, output a list of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-var userInput = 2.3
+var userInput = 2.3;
+var inputValid = false;
+var outputString;
 
-var validate = function(){
+var validateInput = function(){
   parseInt(userInput)
   if (Number.isInteger(userInput)&&((userInput>=1))){
-    return userInput ;
+    return true;
     console.log(userInput);
   } else {
     alert(userInput + " is not valid input.  Please enter a positive integer:");
   };
 };
-validate(userInput);
+
+var transformInput = function(){
+  if (inputValid){
+  for(index = 1; index<=userInput; index++){
+    var transformIndex = index;
+    outputString = outputString + "<li>" + transformIndex + "</li> <br>";
+  };
+  };
+};
